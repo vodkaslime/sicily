f8cf478466ed1fcefb39eadb0a56e916fce28817
jiachen bai <646329483@qq.com>
2021-12-05 00:27:44 -0800

    remove hash.rs

--- src/hash.rs
+++
@@ -1,9 +0,0 @@
-use num::bigint::BigUint;
-use sha2::{ Sha256, Digest };
-
-pub fn digest(input: &String) -> BigUint {
-    let mut hasher = Sha256::new();
-    hasher.update(input.as_bytes());
-    let arr = hasher.finalize();
-    BigUint::from_bytes_be(&arr)
-}
